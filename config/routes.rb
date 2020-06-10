a7efc61c074bfcbfc841dce75937bc200a1c5f45
Hotsukai <kaihotsu.kotaro@gmail.com>
2020-06-10 19:05:12 +0900

    UPDATE routeをリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@
 
 Rails.application.routes.draw do
   root 'lunch#go'
-  # For details on the DSL available within this file, sgetee https://guides.rubyonrails.org/routing.html
   get 'lunch/go'
-  get 'restaurants/index'
-  get 'restaurants/' => 'restaurants#index'
-  get 'restaurants/new'
-  get 'restaurants/:id' => 'restaurants#show'
-  post 'restaurants/create'
+  # get 'restaurants/index'
+  # get 'restaurants/' => 'restaurants#index'
+  # get 'restaurants/new'
+  # get 'restaurants/:id' => 'restaurants#show'
+  # post 'restaurants/create'
+
+  resources :restaurants, only: %i[index show create new]
 end
