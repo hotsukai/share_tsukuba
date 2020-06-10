42e57e270ed24ecfb93f29baf9d82b4646d30a81
Hotsukai <kaihotsu.kotaro@gmail.com>
2020-06-10 19:29:42 +0900

    UPDATE ない店にアクセスしたときに404だす

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  end
 end
