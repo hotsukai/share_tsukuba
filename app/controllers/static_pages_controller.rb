87a9f647f65fd0ddcd6ef04d96fd4b9aa393cb30
Hotsukai <kaihotsu.kotaro@gmail.com>
2020-07-20 09:28:17 +0900

    debug ログインしていないときに新着フォルダが見れないのを解消

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @folder = current_user.folders.build if logged_in?
-      @new_feed_items = Folder.page(params[:page]).per(5)
       @timeline_items = current_user.feed.page(params[:page]).per(5)
     end
+    @new_feed_items = Folder.page(params[:page]).per(5)
   end
 
   def go
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Spot.create(name: 'トタンコットン', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
-Spot.create(name: 'むじゃき', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
-Spot.create(name: 'cafe4', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
-Spot.create(name: '芛堂寺', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
-Spot.create(name: '異国龍', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
-Spot.create(name: 'ガーリーショット', zip: 'hoge', address: 'hogehoge', tel: '000-0000-0000')
+Spot.create(name: "トタンコットン", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "cafe4", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "sora cafe", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "Clematis", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "moikka", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "木村園", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "むじゃき", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "芛堂寺", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "異国龍", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "達郎", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "七福亭", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "俺の生きる道", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "ZEYO", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+
+Spot.create(name: "ガーリーショット", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "うまいもん太陽", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+
+Spot.create(name: "あすかて食堂", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "くい亭", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "好来屋", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "HARE GOHAN", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "フライパン", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "RanRan", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "誠寿司", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
+Spot.create(name: "まつもと", zip: "hoge", address: "hogehoge", tel: "000-0000-0000")
 
 99.times do |n|
-  name  = Faker::Name.name
+  name = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
+  password = "password"
   User.create!(name: name,
                email: email,
                password: password,
@@ -25,13 +45,13 @@ Spot.create(name: 'ガーリーショット', zip: 'hoge', address: 'hogehoge', 
 end
 
 users = User.order(:created_at).take(6)
-50.times do
-  content = 'folder title sample'
+3.times do |n|
+  content = "sample folder #{n}"
   users.each { |user| user.folders.create!(name: content) }
 end
 
 users = User.all
-user  = users.first
+user = users.first
 following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
