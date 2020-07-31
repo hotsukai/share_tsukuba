ffe25394a517fa55463bb963bd58207f94ea944b
Hotsukai <kaihotsu.kotaro@gmail.com>
2020-07-31 13:32:22 +0900

    updateフォルダ作成ボタンをフロートするようになった

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @folder = current_user.folders.build if logged_in?
       @timeline_items = current_user.feed.page(params[:page]).per(5)
     end
     @new_feed_items = Folder.page(params[:page]).per(5)
